91af8981d2cbd0b82c6bf3ba74d4952394eb72a8
kaustubhcs <kaustubhcs07@gmail.com>
2017-06-12 03:32:34 +0530

    Views Updated

--- music/urls.py
+++ music/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # /music/712/
-    url(r'^()$')
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is Music App</h1>")
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
